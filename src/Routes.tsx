2aa00c3190c6ef47921f59741ded6f68cdf57cc0
vanessa <santanavanessa91@gmail.com>
2024-07-21 10:26:20 -0300

    Atualiza os caminhos das páginas no componente `Router` para melhorar a navegação

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { DefaultLayout } from "./layouts/DefaultLayout";
-import { CompleteOrderPage } from "./pages/CompleteOrder";
-import { HomePage } from "./pages/Home";
-import { ConfirmedOrderPage } from "./pages/ConfirmedOrder";
-import AboutPage from "./pages/About";
-import ContactPage from "./pages/Contact";
-import { NotFoundPage } from "./pages/NotFound";
+import { HomePage } from "./../pages/Home";
+import { CompleteOrderPage } from "./../pages/CompleteOrder";
+import { ConfirmedOrderPage } from "./../pages/ConfirmedOrder";
+import AboutPage from "./../pages/About";
+import ContactPage from "./../pages/Contact";
+import { NotFoundPage } from "./../pages/NotFound";
 
 export function Router() {
   return (
