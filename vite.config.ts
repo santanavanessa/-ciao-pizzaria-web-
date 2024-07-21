930ab08d79d2c67dffd74123dedee084028c6f72
vanessa <santanavanessa91@gmail.com>
2024-07-21 14:39:53 -0300

    Atualiza configurações da aplicação

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const isProduction = command ==='build';
-  return {
-    plugins: [react()],
-    base: isProduction ? ' /Phantasma-Dashboard/ ' : '/'
-  };
-});
-
+export default defineConfig({
+  plugins: [react()],
+})
