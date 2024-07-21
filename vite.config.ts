9b9710cbeb463a33083eee1afd9af3c17e3a4e4d
vanessa <santanavanessa91@gmail.com>
2024-07-21 12:30:14 -0300

    Altera as configurações do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ command }) => {
+  const isProduction = command ==='build';
+  return {
+    plugins: [react()],
+    base: isProduction ? ' /Phantasma-Dashboard/ ' : '/'
+  };
+});
+
